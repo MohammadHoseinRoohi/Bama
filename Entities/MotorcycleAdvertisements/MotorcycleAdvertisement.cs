bf4d3d9c55f9a4fdf80ac0bcc1a2392282881c7c
mohammadHoseinRoohi <mohammadhoseinroohi4@gmail.com>
2025-08-29 19:06:32 +0330

    issue#2

---
+++ Entities/MotorcycleAdvertisements/MotorcycleAdvertisement.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Practice4.Entities.Base;
+
+namespace Practice4.Entities.MotorcycleAdvertisements
+{
+    public class MotorcycleAdvertisement : Thing
+    {
+        
+    }
+}
