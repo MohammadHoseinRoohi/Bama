54c2718970398b1a28aaf061c416e8b8aaefcc7d
mohammadHoseinRoohi <mohammadhoseinroohi4@gmail.com>
2025-09-01 00:28:54 +0330

    feature#4

--- Entities/MotorAdvertisings/Motor.cs
+++ Entities/MotorAdvertisings/Motor.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Practice4.Entities.Base;
 
-namespace Practice4.Entities.CarAdvertisements
+namespace Practice4.Entities.MotorAdvertisings
 {
-    public class Car : Thing
+    public class Motor : Thing
     {
         // برند 
         public required string Brand { get; set; }
@@ -14,15 +14,13 @@ namespace Practice4.Entities.CarAdvertisements
         public required string Madel { get; set; }
         // سال
         public double Year { get; set; }
-        // تیپ خودرو
-        public required string VehicleType { get; set; }
+        // نوع سوخت
+        public required string FuelYype { get; set; }
+        // گیربکس
+        public string? Gearbox { get; set; }
         // کارکرد
         public required string Mileage { get; set; }
         // رنگ بدنه
         public required string BodyColor { get; set; }
-        // وضعیت بدنه
-        public required string BodyCondition { get; set; }
-        // رنگ داخلی
-        public required string InteriorColor { get; set; }
     }
 }
--- Entities/MotorAdvertisings/MotorAdvertising.cs
+++ Entities/MotorAdvertisings/MotorAdvertising.cs
@@ -8,6 +8,17 @@ namespace Practice4.Entities.MotorAdvertisings
 {
     public class MotorAdvertising : Thing
     {
-        
+        // کاربر
+        public required User User { get; set; }
+        // موتور
+        public required Motor Motor { get; set; }
+        // قیمت
+        public Price? Price { get; set; }
+        // آدرس عکس 
+        public string? Picture { get; set; }
+        // محل بازدید
+        public required VisitLocation VisitLocation { get; set; }
+        // توضیحات
+        public string? Description { get; set; }
     }
 }
